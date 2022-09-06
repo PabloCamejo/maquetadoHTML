9275e1e1a51e28aed4298c23e1b4db15869fbf5f
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-09-05 23:43:43 -0300

    Agruegue la carpeta de imagenes.Escribi el get y la ruta a estaticos en express.Y por ultimo hice el header en home.html

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const PORT = 3030;
 
 const app = express();
 
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server listen in ${PORT}`);
