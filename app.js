35f826281263bfeabb928f6a5b80769c659610eb
PabloCamejo <pablo.camejo@cencosud.com>
2022-09-06 09:40:46 -0300

    Arreglando el merge

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const PORT = 3030;
-const publicPath = path.resolve(__dirname, './public');
-
 const app = express();
 
+
 const publicPath = path.resolve(__dirname, './public');
+
 app.use( express.static(publicPath) );
 
 app.get('/', (req, res) => {
