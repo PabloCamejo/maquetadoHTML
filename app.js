a7f5c32fb73e91b7667b86e241bfcbd59e70452c
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-09-09 14:44:32 -0300

    Arreglos

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-const PORT = 3030;
+const PORT = 3031;
 const app = express();
 
-
 const publicPath = path.resolve(__dirname, './public');
-
 app.use( express.static(publicPath) );
 
 app.get('/', (req, res) => {
